4cfee7c18378579f843b113b94c81abb86076e8e
Beamtor <adinan.js2002@gmail.com>
2026-06-26 18:48:21 +0700

    set BeamTorTool.rb

---
+++ BeamTorTool.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'sketchup.rb'
+require 'extensions.rb'
+
+require_relative 'src/beam_tor/loader'
--- beam_tor_tool.rb
+++
@@ -1,9 +0,0 @@
-module BeamTorTool
-  module Extension
-    def self.activate
-      puts "BeamTorTool extension loaded"
-    end
-  end
-end
-
-BeamTorTool::Extension.activate
