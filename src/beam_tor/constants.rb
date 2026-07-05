ded28136340d9bbe08900ae1fecff3969b845a93
Beamtor <adinan.js2002@gmail.com>
2026-07-05 12:27:47 +0700

    update

---
+++ src/beam_tor/constants.rb
@@ -0,0 +1,4 @@
+module BeamTorTool
+  PLUGIN_NAME = 'Beam Tor Tool'.freeze
+  PLUGIN_AUTHOR = 'Adinan Chansaeng (Tor)'.freeze
+end
--- src/beam_tor/loader.rb
+++ src/beam_tor/loader.rb
@@ -1,33 +1,16 @@
-# frozen_string_literal: true
-
-<<<<<<< HEAD:src/beam_tor/loader.rb
+require 'sketchup.rb'
 require_relative 'version'
 require_relative 'constants'
-require_relative 'menu'
-require_relative 'toolbar'
 
-module BeamTor
-
-  puts "=================================="
-  puts EXTENSION_NAME
-  puts "Version : #{VERSION}"
-  puts "Loaded Successfully"
-  puts "=================================="
-
-  Menu.create
-  Toolbar.create
-
-end
-=======
-require_relative "beam_tor"
-
-module BeamTor
-
-  def self.load
-    puts "Beam Tor Tool v#{VERSION} Loaded"
+module BeamTorTool
+  unless file_loaded?(__FILE__)
+    
+    puts "========================================"
+    puts "#{PLUGIN_NAME} v#{VERSION} loaded successfully!"
+    puts "Developer: #{PLUGIN_AUTHOR}"
+    puts "Status: Milestone 1 - Framework Loaded"
+    puts "========================================"
+    
+    file_loaded(__FILE__)
   end
-
 end
-
-BeamTor.load
->>>>>>> parent of 48695e0 (Refactor project structure):src/loader.rb
