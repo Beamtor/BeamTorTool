1ffaae1ed3b5c3cfc04b820bc343a3cd3c2afca8
Beamtor <adinan.js2002@gmail.com>
2026-07-05 19:37:36 +0700

    up

--- BeamTorTool/src/beam_tor/loader.rb
+++ BeamTorTool/src/beam_tor/loader.rb
@@ -2,18 +2,18 @@ require 'sketchup.rb'
 require_relative 'version'
 require_relative 'constants'
 require_relative 'menu'
+require_relative 'toolbar'
 
 module BeamTorTool
   unless file_loaded?(__FILE__)
     
-    # เรียกฟังก์ชันสร้างเมนู
     Menu.create
+    Toolbar.create
     
-    # พิมพ์สถานะลง Console
     puts "========================================"
     puts "#{PLUGIN_NAME} v#{VERSION} loaded successfully!"
     puts "Developer: #{PLUGIN_AUTHOR}"
-    puts "Status: Milestone 2 - Menu Created"
+    puts "Status: Milestone 3 - Toolbar Created"
     puts "========================================"
     
     file_loaded(__FILE__)
